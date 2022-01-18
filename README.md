cddd4375635b384009605dbc5e1ed46742d53b11
stein-88 <wanderson.code@icloud.com>
2022-01-18 12:08:56 -0300

    checker in the ES2015+ parameters

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # chekernew
+
+## Objetivo
+
+Atraves desse repositório você poderá
+- Verificar se existe algum erro na sintaxe dos seguintes formatos:
+`HTML, CSS, JavaScript e JSON`
+- Minificar todo o conteúdo dos formatos, exceto JSON.
+- Atualizar em tempo real enquanto gera o código.
+- Deixa seu código pronto para ser usado em `production`.
+
+## Install
+
+`yarn install`
+
+## Usage
+
+- Os arquivos HTML devem estar na pasta `filestocheck/htmls`
+- Os arquivos CSS devem estar na pasta `filestocheck/csss`
+- Os arquivos JSON devem estar na pasta `filestocheck/jsons`
+- Os arquivos JavaScript devem estar na pasta `filestocheck/jss`
+
+- Arquivos minificados `.html` vao para a pasta `assets/`.
+- Arquivos minificados `.css` vao para a pasta `assets/style/`.
+- Arquivos minificados `.js` e `.json` vao para a pasta `assets/scripts/`.
+
+Você pode rodar os seguintes comandos:
+- `yarn checkhtml` para verificar arquivos HTML.
+- `yarn checkcss` para verificar arquivos CSS.
+- `yarn checkjs` para verificar arquivos JavaScript.
+- `yarn checkjson` para verificar arquivos JSON.
+- `yarn watcher` para verificar alterações nos arquivos em tempo real.
+- `yarn clean` para deletar a pasta assets, que recebe todo o conteúdo gerado a partir das verficações.
+- `yarn build` limpa a pasta assets, em paralelo executa a verificação da sintaxe, minimifica os arquivos e coloca na pasta assets conforme o padrão de cada formato.
+- `yarn default` executa o comando build como padrão.
+
+Caso apresente um erro de HTML que voce deseja ignorar ultilize o arquivo .htmllintrc. Voce pode verificar a lista de exclusoes no seguinte [website](https://github.com/htmllint/htmllint/wiki/Options)
+
+## Configurações para ES2015+
+
+A pasta `gulpfile.js` passa a ser `gulpfile.babel.js`
+
+As importações são feitas nesse formato: `import gulp from 'gulp'`
+Assim como no exemplo a seguir você poderá exportar direto sua função: `export const clean = () => del([ 'assets' ])`
+
+Para evitar problemas de compatibilidade nós sugerimos a criação de um arquivo `.babelrc` com a seguinte configuração.
+`{
+  "presets": [ "@babel/preset-env" ]
+}`
+
+Será necessário também a criação de um arquivo `.eslintrc` com a seguinte configuração.
+`{}` para mais [informações](https://github.com/adametry/gulp-eslint)
+
+O documento completo para maior [orientação](https://github.com/gulpjs/gulp)
---
+++ gulpfile.babel.js
@@ -0,0 +1,96 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import concat from 'gulp-concat'
+import uglify from 'gulp-uglify'
+import cleanCSS from 'gulp-clean-css'
+import del from 'del'
+import jsonlint from"gulp-jsonlint"
+import log from'fancy-log'
+import ohhtml from'gulp-htmllint'
+import colors from'ansi-colors'
+import wxmlmin from'gulp-wxml-min'
+import eslint from'gulp-eslint'
+
+export const paths = {
+    js: {
+        src: './filestocheck/jss/*.js',
+        dest: './assets/scripts'
+    },
+    json: {
+        src: './filestocheck/jsons/*.json',
+        dest: './assets/scripts'
+    },
+    css: {
+        src: './filestocheck/csss/*.css',
+        dest: './assets/style'
+    },
+    html: {
+        src: './filestocheck/htmls/*.html',
+        dest: './assets'
+    }
+}
+
+export const clean = () => {
+    return del(['assets'])
+}
+
+export const checkcss = () => {
+    return gulp.src(paths.css.src)
+        .pipe(cleanCSS())
+        .pipe(concat('main.min.css'))
+        .pipe(gulp.dest(paths.css.dest))
+}
+
+export const myCustomReporter = (file) => {
+    log('File ' + file.path + ' is not valid JSON.')
+}
+
+export const checkjson = () => {
+    return gulp.src(paths.json.src)
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter(myCustomReporter))
+        .pipe(gulp.dest(paths.json.dest))
+}
+
+export const htmllintReporter = (filepath, issues) => {
+    if (issues.length > 0) {
+        issues.forEach((issue) => {
+            log(colors.cyan('[gulp-htmllint] ') + colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + colors.red('(' + issue.code + ') ' + issue.msg))
+        })
+        process.exitCode = 1
+    }
+}
+
+export const checkhtml = () => {
+    return gulp.src(paths.html.src)
+        .pipe(ohhtml({}, htmllintReporter))
+        .pipe(wxmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            keepClosingSlash: true
+        }))
+        .pipe(gulp.dest(paths.html.dest))
+}
+
+export const checkjs = () => {
+    return gulp.src(paths.js.src, { sourcemaps: true })
+        .pipe(babel())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest(paths.js.dest))
+}
+
+export const watcher = () => {
+    gulp.watch(paths.js.src, checkjs)
+    gulp.watch(paths.css.src, checkcss)
+    gulp.watch(paths.json.src, checkjson)
+    gulp.watch(paths.html.src, checkhtml)
+    return null
+}
+
+export const build = gulp.series(clean, gulp.parallel(checkjs, checkjson, checkhtml, checkcss))
+
+export default build
